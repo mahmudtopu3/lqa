930d531a3c3a686c94d90ffb34969ae762da80f3
mahmudtopu3 <mahmudtopu3@gmail.com>
2021-01-20 00:08:42 +0600

    faker seeder user and questions

---
+++ database/factories/QuestionFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Question::class, function (Faker $faker) {
+    return [
+        'title' => rtrim($faker->sentence(rand(5,10)),'.'),  /* remove traling dot */
+        'body'  => $faker->paragraphs(rand(3,7),true), /* make array into single paragraph with new lines */
+        'views'  => rand(0,10),
+        'answers'  => rand(0,10),
+        'votes'  => rand(-3,10)
+    ];
+});
+
+
+/* 
+In tinker write
+# factory(App\User::class,3)->create() To make 3 fake data 
+# You can put this code on run method of database seed class
+
+*/
