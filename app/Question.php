0687a1fbf8279362e2e7ab7237fd59efb3eb8e2d
mahmudtopu3 <mahmudtopu3@gmail.com>
2021-01-20 23:41:51 +0600

    answer schema doctrine alter table questiopn

--- app/Question.php
+++ app/Question.php
@@ -25,7 +25,7 @@ class Question extends Model
         return $this->created_at->diffForHumans();
     }
     public function getStatusAttribute(){
-        if($this->answers >0 ){
+        if($this->answers_count >0 ){
             if($this->best_abswer_id){
                 return "answered-accepted";
             }
@@ -36,6 +36,10 @@ class Question extends Model
 
     public function getBodyHtmlAttribute(){
         return \Parsedown::instance()->text($this->body);
+    }
+
+    public function answers(){
+        return $this->hasMany(Answer::class); 
     }
 
 
--- app/User.php
+++ app/User.php
@@ -36,6 +36,10 @@ class User extends Authenticatable
         //return route('questions.show',$this->id);
         return "#";
     }
+
+    public function answers(){
+        return $this->hasMany(Answer::class); 
+    }
     
     
 }
