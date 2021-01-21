aadb547df727f00c685b36e935ad75e5cdbae803
mahmudtopu3 <mahmudtopu3@gmail.com>
2021-01-21 20:28:39 +0600

    answer vote system ui fontawesome install npm package with scss variable

--- resources/views/questions/view.blade.php
+++ resources/views/questions/view.blade.php
@@ -5,7 +5,8 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header">
+               <div class="card-body">
+               <div class="card-title">
                     <div class="d-flex align-items-center">
                         <h1>{{ $question->title }}</h1>
                         <div class="ml-auto">
@@ -14,10 +15,27 @@
                     </div>
                     
                 </div>
+                <hr>
 
-                <div class="card-body">
-                   {!! $question->body_html !!}
-                   <div class="float-right">
+
+
+                <div class="media">
+                    <div class="d-flrex flex-column vote-controls">
+                        <a title="This question is useful"href="" class="vote-up">
+                          <i class="fas fa-caret-up fa-3x"></i>
+                        </a>
+                        <span class="votes-count">5</span>
+                        <a title="This question is not useful"href="" class="vote-up off">
+                        <i class="fas fa-caret-down fa-3x"></i>
+                        </a>
+                        <a title="Click to mark as facorite quesiton(click again to undo)" class="favorite mt-2 favorited">
+                        <i class="fas fa-star fa-3x"></i> 
+                        <span class="favorites-count">123</span>
+                        </a>
+                    </div>
+                  <div class="media-body">
+                    {!! $question->body_html !!}
+                     <div class="float-right">
                         <span class="text-muted">Answered {{ $question->created_date }}</span>
                         <div class="media mt-2">
                             <a href="{{ $question->user->url }}" class="pr-2">
@@ -28,7 +46,11 @@
                             </div>
                         </div>
                     </div>
+                  </div>
+               
                 </div>
+               </div>
+           
             </div>
         </div>
     </div>
@@ -42,6 +64,19 @@
                     <hr>
                     @foreach ($question->answers as $answer)
                         <div class="media">
+                        <div class="d-flrex flex-column vote-controls">
+                            <a title="This answer is useful"href="" class="vote-up">
+                              <i class="fas fa-caret-up fa-3x"></i>
+                            </a>
+                            <span class="votes-count">5</span>
+                            <a title="This answer is not useful"href="" class="vote-up off">
+                            <i class="fas fa-caret-down fa-3x"></i>
+                            </a>
+                            <a title="Mark this answer as best answer" class="vote-accepted mt-2 ">
+                            <i class="fas fa-check fa-2x"></i> 
+                            
+                            </a>
+                        </div>
                             <div class="media-body">
                                 {!! $answer->body_html !!}
                                 <div class="float-right">
@@ -64,4 +99,4 @@
         </div>
     </div>
 </div>
-@endsection
+@endsection 
