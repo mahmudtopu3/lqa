598aac8d7f3dab8fe14d4fe3480c15dd49c7337e
mahmudtopu3 <mahmudtopu3@gmail.com>
2021-01-21 23:52:05 +0600

    best answer id deleted event

--- app/Answer.php
+++ app/Answer.php
@@ -24,10 +24,15 @@ class Answer extends Model
         parent::boot();
         static::created(function($answer){
             $answer->question->increment('answers_count');
-            
+             
         });
         static::deleted(function($answer){
-            $answer->question->decrement('answers_count');
+            $question = $answer->question;
+            $question->decrement('answers_count');
+            if($question->best_answer_id==$answer->id){
+                $question->best_answer_id = NULL;
+                $question->save();
+            }
            
         });
         
@@ -35,5 +40,9 @@ class Answer extends Model
 
     public function getCreatedDateAttribute(){
         return $this->created_at->diffForHumans();
+    }
+
+    public function getStatusAttribute(){
+        return $this->id===$this->question->best_answer_id ? 'vote-accepted' : '';
     }
 }
